fb689feed7869900ba2c3f4ff8c7a41d3d6e0c74
Rickanjo <Ricardo.cassettari07@gmail.com>
2023-04-01 15:20:58 -0300

    Exercício Resolvido

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// Receba uma medida em metros e exiba seus equivalentes em quilômetros e centímetros.
+
+double metros;
+double Km;
+double cm;
+
+Console.WriteLine("----Conversor de Equivalêcia----\n");
+
+Console.Write("Digite a medida em metros: ");
+metros = double.Parse(Console.ReadLine()!);
+
+Km = metros / 1000;
+cm = metros * 100;
+
+Console.WriteLine($"\n{metros} equivalente a {Km} quilômetros e {cm} centímetros");
